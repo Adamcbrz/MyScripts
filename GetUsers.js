4848673cf54f4fbf3429250f340499f551230fb3
Adam Brzozowski <adam.brzozowski@bottlerocketapps.com>
2016-01-22 11:53:34 -0600

    Add Get Users Cloud Script

---
+++ GetUsers.js
@@ -0,0 +1,10 @@
+handlers.GetUsers = function (args) {
+	var playerids = args.playerids;
+	var playersData = [];
+	for(var i = 0; i < playerids.length; i++)
+	{
+         playersData[i] = {server.GetUserAccountInfo({PlayFabId: playerids[i]}).Data};
+	}
+	
+	return playersData;
+}
