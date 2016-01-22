aec6fa968ee5d927d17f338710dad5bff348304a
Adam Brzozowski <adam.brzozowski@bottlerocketapps.com>
2016-01-22 12:13:20 -0600

    Testing

--- GetUsers.js
+++ GetUsers.js
@@ -1,10 +1,11 @@
 handlers.GetUsers = function (args) {
 	var playerids = args.playerids;
-	var playersData = [];
-	for(var i = 0; i < playerids.length; i++)
-	{
-         playersData[i] = server.GetUserAccountInfo({PlayFabId: playerids[i]}).Data;
-	}
+	return playerids;
+	//var playersData = [];
+	//for(var i = 0; i < playerids.length; i++)
+	//{
+    //     playersData[i] = server.GetUserAccountInfo({PlayFabId: playerids[i]}).Data;
+	//}
 	
-	return playersData;
+//	return playersData;
 }
